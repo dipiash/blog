7bea996fb7071f26b04f10a2ccaff016c16bc4b9
dipiash <dipiash@users.noreply.github.com>
2015-09-18 22:25:57 +0300

    Added method for checked selected link

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,27 @@
 module PagesHelper
+
+  def nav_tab_selected
+    links = { home: '' , links: '', faq: '', curriculumvitae: ''}
+    selected = 'nav_items__item--selected'
+
+    if current_page?(controller: 'pages', action: 'home')
+      links[:home] = selected
+      return links
+    end
+
+    if current_page?(controller: 'pages', action: 'links')
+      links[:links] = selected
+      return links
+    end
+
+    if current_page?(controller: 'pages', action: 'faq')
+      links[:faq] = selected
+      return links
+    end
+
+    if current_page?(controller: 'pages', action: 'curriculumvitae')
+      links[:curriculumvitae] = selected
+      return links
+    end
+  end
 end
