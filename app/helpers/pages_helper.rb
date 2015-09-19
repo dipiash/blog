3659355489c43045f23fd9540b8b4b886ee5c1fa
dipiash <dipiash@users.noreply.github.com>
2015-09-19 20:49:41 +0300

    Added begin/rescue

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,24 +4,28 @@ module PagesHelper
     links = { home: '' , links: '', faq: '', curriculumvitae: ''}
     selected = 'nav_items__item--selected'
 
-    if current_page?(controller: 'pages', action: 'home')
-      links[:home] = selected
-      return links
-    end
+    begin
+      if current_page?(controller: 'pages', action: 'home')
+        links[:home] = selected
+        return links
+      end
 
-    if current_page?(controller: 'pages', action: 'links')
-      links[:links] = selected
-      return links
-    end
+      if current_page?(controller: 'pages', action: 'links')
+        links[:links] = selected
+        return links
+      end
 
-    if current_page?(controller: 'pages', action: 'faq')
-      links[:faq] = selected
-      return links
-    end
+      if current_page?(controller: 'pages', action: 'faq')
+        links[:faq] = selected
+        return links
+      end
 
-    if current_page?(controller: 'pages', action: 'curriculumvitae')
-      links[:curriculumvitae] = selected
-      return links
+      if current_page?(controller: 'pages', action: 'curriculumvitae')
+        links[:curriculumvitae] = selected
+        return links
+      end
+    rescue
+      links = { home: '' , links: '', faq: '', curriculumvitae: ''}
     end
   end
 end
