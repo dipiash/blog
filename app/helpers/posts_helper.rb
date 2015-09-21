f4e07c271d050e7a020bfd52833bed03959708a4
dipiash <dipiash@users.noreply.github.com>
2015-09-21 18:55:03 +0300

    Added a method for remove a trncate_text tag

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,4 +3,8 @@ module PostsHelper
     index = post_content =~ /&lt;truncate_text&gt;/ || post_content.length
     post_content[0,index]
   end
+
+  def remove_truncate_text(post_content)
+    post_content.gsub!("&lt;truncate_text&gt;", "")
+  end
 end
