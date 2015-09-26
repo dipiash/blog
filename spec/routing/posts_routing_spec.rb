bffd003b25636986007494731e9d1edc80555b96
dipiash <dipiash@users.noreply.github.com>
2015-09-26 19:40:44 +0300

    writed a first test

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -1,1 +1,16 @@
 require "rails_helper"
+
+describe PostsController do
+  let!(:user) { FactoryGirl.create(:user) }
+
+  it "should be login is true" do
+    visit '/login'
+    expect(page).to have_title('Dipiash | Вход')
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: '00000000'
+    click_on 'Войти'
+    expect(page).to have_title('Dipiash | Новости')
+    visit '/posts'
+    expect(page).to have_title('Dipiash | Список записей')
+  end
+end
