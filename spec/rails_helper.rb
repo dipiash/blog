cbfeee490758b73de571bc6582db286748d1b88e
dipiash <dipiash@users.noreply.github.com>
2015-09-27 19:00:24 +0300

    Updated

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,19 @@
 require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
 
+require 'devise'
+
 require 'capybara/rspec'
 require 'capybara/rails'
+
+CodeClimate::TestReporter.start
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../../config/environment', __FILE__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -57,5 +62,5 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Capybara::DSL
 
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::TestHelpers, :type => :controller
 end
