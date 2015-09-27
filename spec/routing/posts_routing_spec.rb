77ce2a3c369a8b8f163e4a4ffbfb6a6a4111ff63
dipiash <dipiash@users.noreply.github.com>
2015-09-27 17:33:51 +0300

    Added a new test

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -2,15 +2,24 @@ require "rails_helper"
 
 describe PostsController do
   let!(:user) { FactoryGirl.create(:user) }
+  let!(:post) { FactoryGirl.create(:post, title: "TEST01") }
 
-  it "should be login is true" do
+  before do
     visit '/login'
     expect(page).to have_title('Dipiash | Вход')
     fill_in 'Email', with: user.email
-    fill_in 'Password', with: '00000000'
+    fill_in 'Password', with: user.password
     click_on 'Войти'
+  end
+
+  it "should be login is true" do
     expect(page).to have_title('Dipiash | Новости')
     visit '/posts'
     expect(page).to have_title('Dipiash | Список записей')
+  end
+
+  it "should validt title for post" do
+    visit "posts/#{post.id}"
+    expect(page).to have_title('Dipiash | TEST01')
   end
 end
