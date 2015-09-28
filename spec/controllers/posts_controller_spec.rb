c69163a86307fc879c517c2483ce825a8b2e709a
dipiash <dipiash@users.noreply.github.com>
2015-09-28 19:28:12 +0300

    Writed a new test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -12,7 +12,7 @@ describe "tt" do
     expect(page).to have_title('Dipiash | Новости')
   end
 
-  it '#new' do
+  it '#new create post and show full' do
     visit 'posts/new'
     expect(page).to have_title('Dipiash | Новая запись')
     fill_in 'Название', with: 'Example title for create'
@@ -20,5 +20,16 @@ describe "tt" do
     click_on 'Подтвердить'
     expect(page).to  have_title('Dipiash | Example title for create')
     expect(page).to have_content('Example content Example hide text')
+  end
+
+  it '#new create post and show preview' do
+    visit 'posts/new'
+    expect(page).to have_title('Dipiash | Новая запись')
+    fill_in 'Название', with: 'Example title for create'
+    fill_in 'Текст записи', with: 'Example content &lt;truncate_text&gt; Example hide text'
+    click_on 'Подтвердить'
+    visit '/'
+    expect(page).to  have_title('Dipiash | Новости')
+    expect(page).to have_content('Example content ...')
   end
 end
