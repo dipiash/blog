1a76d32d97860463bd145a1845947cea07fb6dfc
dipiash <dipiash@users.noreply.github.com>
2015-09-29 20:05:21 +0300

    Added open graph metadata

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     content_for(:title) { page_title }
   end
 
+  def opengraph_config(page_title)
+    { og_type: "website",
+      og_title: page_title,
+      og_description: "Dipiash. Блог о веб разработке и нетолько.",
+      og_url: request.original_url
+    }
+  end
+
 end
