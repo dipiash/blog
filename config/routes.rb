dff800b327792b1e817ecde632df27af3d81d052
dipiash <dipiash@users.noreply.github.com>
2015-09-29 21:16:37 +0300

    Added a new rout for rss feed

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   match 'faq', to: 'pages#faq', via: :get
   match 'feed', to: 'posts#feed', via: :get
+  match 'rss', to: 'posts#rss', via: :get
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
