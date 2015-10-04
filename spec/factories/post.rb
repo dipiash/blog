abfe063aacdad9997fb1b5be9abd21c74b864245
dipiash <dipiash@users.noreply.github.com>
2015-10-04 15:29:15 +0300

    Fixed error

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :post do
     sequence(:title) { |n| "title#{n}" }
-    sequence(:content) { |n| "content{n} <truncate_text> lorem ipsum jdfslgjfdgkl" }
+    sequence(:content) { |n| "content#{n} <truncate_text> lorem ipsum jdfslgjfdgkl" }
+    sequence(:tag_list) { |n| "tag#{n}, tag#{n+1}, tag#{n+2}"}
     sequence(:created_at) { Time.now }
     sequence(:updated_at) { Time.now }
   end
